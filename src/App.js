dcbbca1db6890b276c4b96957267e634b766dc63
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2022-11-22 23:27:53 +0000

    Import Home component and display it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Home />
     </div>
   );
 }

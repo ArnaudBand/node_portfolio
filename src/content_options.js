6a57f5f057ea94b263c60571770782229520bd6f
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2022-11-22 23:26:53 +0000

    Add image and intro

--- src/content_options.js
+++ src/content_options.js
@@ -1,6 +1,8 @@
 const options = {
   title: 'Arnaud BAND',
-  description: "I'm a software engineer, I love to code and I'm passionate about new technologies.",
+  description: "I'm a software developer. I love to code and to learn new things.",
+  image: 'https://png.pngtree.com/png-clipart/20220727/ourmid/pngtree-red-face-with-scar-png-format-png-image_6088900.png',
+  intro: 'Hi, I’m Arnaud BAND, a software developer. I’m passionate about new technologies and I love to code. I’m currently working at <a href="https://www.ekino.com/" target="_blank">Ekino</a> as a Senior Software Engineer.',
 };
 
 export default options;

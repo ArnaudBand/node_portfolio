b17ab411d80faeb42fab914893fbe44a6ce8ef0d
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2022-11-22 16:16:02 +0000

    Create routes to get all components

---
+++ src/routes.js
@@ -0,0 +1,24 @@
+const routes = [
+  {
+    id: 1,
+    link: 'Home',
+  },
+  {
+    id: 2,
+    link: 'About',
+  },
+  {
+    id: 3,
+    link: 'Skills',
+  },
+  {
+    id: 4,
+    link: 'Portfolio',
+  },
+  {
+    id: 5,
+    link: 'Contact',
+  },
+];
+
+export default routes;

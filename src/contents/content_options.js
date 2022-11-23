87b6388c70208013eeb243e3051613f22ad50861
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2022-11-23 08:55:02 +0000

    Add sociallink on the content_options

--- src/contents/content_options.js
+++ src/contents/content_options.js
@@ -3,6 +3,12 @@ const options = {
   description: "I'm a software developer. I love to code and to learn new things.",
   image: 'https://png.pngtree.com/png-clipart/20220727/ourmid/pngtree-red-face-with-scar-png-format-png-image_6088900.png',
   intro: 'Hi, I’m Arnaud BAND, a software developer. I’m passionate about new technologies and I love to code. I’m currently working at <a href="https://www.ekino.com/" target="_blank">Ekino</a> as a Senior Software Engineer.',
+  social: {
+    twitter: 'https://twitter.com/@ba104781',
+    github: 'https://github.com/ArnaudBand',
+    linkedin: 'https://www.linkedin.com/in/ArnaudBandonkeye/',
+    email: 'bandonkeyea@gmail.com',
+  },
 };
 
 export default options;

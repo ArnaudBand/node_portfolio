6eb08728556da136cffb73583f67aed7b84f4628
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2022-11-23 08:26:58 -0600

    Add logo and photo

--- src/contents/content_options.js
+++ src/contents/content_options.js
@@ -1,7 +1,8 @@
 const options = {
+  logo: '../assets/logo.png',
   title: 'Arnaud BAND',
   description: "I'm a software developer. I love to code and to learn new things.",
-  image: 'https://png.pngtree.com/png-clipart/20220727/ourmid/pngtree-red-face-with-scar-png-format-png-image_6088900.png',
+  image: '../assets/author.jpg',
   intro: 'Hi, I’m Arnaud BAND, a software developer. I’m passionate about new technologies and I love to code. I’m currently working at <a href="https://www.ekino.com/" target="_blank">Ekino</a> as a Senior Software Engineer.',
   social: {
     twitter: 'https://twitter.com/@ba104781',

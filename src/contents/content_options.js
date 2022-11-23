16e94df6f523bd9861ea5c820e766fde9bf12e12
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2022-11-23 14:45:21 +0000

    Add image using a link

--- src/contents/content_options.js
+++ src/contents/content_options.js
@@ -1,8 +1,8 @@
 const options = {
-  logo: '../assets/logo.png',
+  logo: 'https://ik.imagekit.io/a9w6aihek/logo11_20_8715.png?ik-sdk-version=javascript-1.4.3&updatedAt=1669214534908',
   title: 'Arnaud BAND',
   description: "I'm a software developer. I love to code and to learn new things.",
-  image: '../assets/author.jpg',
+  image: 'https://ik.imagekit.io/a9w6aihek/Screenshot_20221116-184048_Gallery.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1669214536792',
   intro: 'Hi, I’m Arnaud BAND, a software developer. I’m passionate about new technologies and I love to code. I’m currently working at <a href="https://www.ekino.com/" target="_blank">Ekino</a> as a Senior Software Engineer.',
   social: {
     twitter: 'https://twitter.com/@ba104781',

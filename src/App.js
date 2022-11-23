67607ce6335f2b43d569b622440986b2dd8c0131
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2022-11-23 00:22:44 +0000

    Import the socialLink and display it

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import SocialLinks from './components/socialLink';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Home />
+      <SocialLinks />
     </div>
   );
 }

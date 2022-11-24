d73426fc6a46f239cf980b904a9b30b96c790949
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2022-11-24 17:57:06 +0000

    Use github Icon

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
 import { FcGlobe } from 'react-icons/fc';
-import { MdLiveTv } from 'react-icons/md';
+import { DiGithubFull } from 'react-icons/di';
 import options from '../contents/content_options';
 
 const Portfolio = () => (
   <div
-    className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen"
+    name="portfolio"
+    className="w-full bg-gradient-to-b from-black to-gray-800 text-white py-20"
   >
     <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-      <div className="pb-8">
-        <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+      <div>
+        <p className="text-4xl py-11 font-bold inline underline underline-offset-8 border-gray-500">
           Portfolio
         </p>
-        <p className="py-6">Check out some of my work right here</p>
+        <p className="py-5">Check out some of my work right here</p>
       </div>
 
       <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
@@ -22,15 +23,15 @@ const Portfolio = () => (
           <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
             <img
               src={src}
-              alt=""
-              className="rounded-md duration-200 hover:scale-105"
+              alt="portfolio"
+              className="rounded-md duration-200 hover:scale-105 h-48"
             />
             <div className="flex items-center justify-center">
               <a href={live} className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                <MdLiveTv />
+                <FcGlobe className="text-2xl" />
               </a>
               <a href={github} className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
-                <FcGlobe />
+                <DiGithubFull className="text-4xl" />
               </a>
             </div>
           </div>

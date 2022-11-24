c430d6860c0ae917f7e69f7ba84db96add8d4c70
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2022-11-24 12:22:01 +0000

    Add portfolio contents

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -2,8 +2,37 @@ import React from 'react';
 import options from '../contents/content_options';
 
 const Portfolio = () => (
-  <div className="portfolio">
-    <h1>Portfolio</h1>
+  <div
+    className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen"
+  >
+    <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+      <div className="pb-8">
+        <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+          Portfolio
+        </p>
+        <p className="py-6">Check out some of my work right here</p>
+      </div>
+
+      <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+        {options.portfolios.map(({ id, src }) => (
+          <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
+            <img
+              src={src}
+              alt=""
+              className="rounded-md duration-200 hover:scale-105"
+            />
+            <div className="flex items-center justify-center">
+              <button type="button" className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                Demo
+              </button>
+              <button type="button" className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                Code
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
   </div>
 );
 

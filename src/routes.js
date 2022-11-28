14c050efd12465cc98d3a1516ac185b0d51a5cae
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2022-11-28 15:24:08 -0600

    Add style as root

--- src/routes.js
+++ src/routes.js
@@ -10,30 +10,35 @@ const routes = [
     path: '/',
     link: 'home',
     element: <Home />,
+    style: '--i: 1, --clr: #f5f5f5',
   },
   {
     id: 2,
     path: '/About',
     link: 'about',
     element: <About />,
+    style: '--i: 2, --clr: #f5f5f5',
   },
   {
     id: 3,
     path: '/Skills',
     link: 'skills',
     element: <Skills />,
+    style: '--i: 3, --clr: #f5f5f5',
   },
   {
     id: 4,
     path: '/Portfolio',
     link: 'portfolio',
     element: <Portfolio />,
+    style: '--i: 4, --clr: #f5f5f5',
   },
   {
     id: 5,
     path: '/Contact',
     link: 'contact',
     element: <Contact />,
+    style: '--i: 5, --clr: #f5f5f5',
   },
 ];
 

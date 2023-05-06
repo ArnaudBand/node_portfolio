1ebfc0defd4dd6efee88d3dc627355f95c66500c
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2023-05-06 03:38:33 +0300

    Replace text with an image

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { FaBars, FaTimes } from 'react-icons/fa';
+import options from '../contents/content_options';
 import routes from '../routes';
 
 const Navbar = () => {
@@ -9,7 +10,7 @@ const Navbar = () => {
   return (
     <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
       <div>
-        <h1 className="text-5xl font-signature ml-2">Arnaud</h1>
+        <img src={options.image_logo} height="75" width="75" alt="" />
       </div>
 
       <ul className="hidden md:flex">
